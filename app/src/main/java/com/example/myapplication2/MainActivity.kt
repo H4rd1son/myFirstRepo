637e9ccb99c27a04cb1eb8229d74eaca8cd49ed1
Hardison <hardison@xaker.ru>
2023-02-15 18:17:44 +0300

    15.02.2023 add EditText, setText from EditText to textView, add Button for EditText 15.02.2023 rename var a:Int = 0 to var count

--- app/src/main/java/com/example/myapplication2/MainActivity.kt
+++ app/src/main/java/com/example/myapplication2/MainActivity.kt
@@ -18,30 +18,30 @@ class MainActivity : AppCompatActivity() {
         val button1: View = findViewById(R.id.button1)
         val button2: View = findViewById(R.id.button2)
         val button3: View = findViewById(R.id.buttonReset)
-        var a:Int=0
+        var count:Int=0
         val funLogs = Logs()
         val textView: TextView = findViewById(R.id.textHelloWorld)
 
         button1.setOnClickListener {
-            a--
-            textView.text = "нажато минус: число равно = $a"
+            count--
+            textView.text = "нажато минус: число равно = $count"
             funLogs.button1Log(button1)
         }
         button2.setOnClickListener {
-            a++
-            textView.text = "нажато плюс: число равно = $a"
+            count++
+            textView.text = "нажато плюс: число равно = $count"
             funLogs.button2Log(button2)
         }
         button3.setOnClickListener {
-            a = 0
-            textView.text = "нажат сброс: число равно = $a"
+            count = 0
+            textView.text = "нажат сброс: число равно = $count"
             funLogs.button3Log(button3)
         }
         val edText: EditText = findViewById(R.id.editText1)
         val button4: View = findViewById(R.id.button4)
         button4.setOnClickListener {
             val text: String = edText.getText().toString()
-            textView.setText("$text $a")
+            textView.setText("$text $count")
         }
     }
 }
