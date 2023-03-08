61dae48aeb21859616e9dfc7b7df4c3378429bd0
Hardison <hardison@xaker.ru>
2023-03-08 19:57:40 +0300

    15.02.2023 add EditText, setText from EditText to textView, add Button for EditText 15.02.2023 rename var a:Int = 0 to var count 15.02.2023 add Log fun for button4 17.02.2023 add AlertDialog to button4 20.02.2023 add translate for @+id/editText1, add 2nd activity 21.02.2023 неосилил пока Basic Acticvity, необходимо разобраться с переходами между активностями, добавил EmptyActivity, пока потренируюсь на нем 27.02.2023 Стёр кнопки со второй активити, добавил фрагмент и логи на вторую активити 02.03.2023 Удалил все неиспользуемые и нерабочие классы 06.03.2023 воспользовался ViewBinding, создал класс для разметки, используя lateinit var bindingClass. Переписал некоторые кнопки используя созданный клас. Избавился от локальных переменных в классе MainActivity 08.03.2023 убрал фрагмент со второго активити, добавил кнопку, 2 textView, EditText. При вводе числа, в зависимости от числа отображается текст.

--- app/src/main/java/com/example/myapplication2/SecondEmptyActivity.kt
+++ app/src/main/java/com/example/myapplication2/SecondEmptyActivity.kt
@@ -7,23 +7,47 @@ import android.util.Log
 import android.view.View
 import android.widget.*
 import androidx.appcompat.app.AlertDialog
+import com.example.myapplication2.databinding.ActivityMainBinding
+import com.example.myapplication2.databinding.SecondEmptyActivityBinding
 
 class SecondEmptyActivity : AppCompatActivity() {
+
+    lateinit var bindingClass : SecondEmptyActivityBinding
 
     companion object {
         private const val TAG = "SecondEmptyActivity"
     }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.second_empty_activity)
+        bindingClass = SecondEmptyActivityBinding.inflate(layoutInflater)
+//        setContentView(R.layout.second_empty_activity)
+        setContentView(bindingClass.root)
 
         Log.d(TAG, "onCreate")
 
-        val buttonFirstScreen: View = findViewById(R.id.buttonPreviousActivity)
-        buttonFirstScreen.setOnClickListener {
+        bindingClass.acceptNumberButton.setOnClickListener {
+            val count = bindingClass.inputNumberField.getText().toString().toInt()
+            if(count >= 1000) {
+                bindingClass.textWhoAreYou.text = "красава у тебя $count подписчиков, но ты всё равно лох" //если 1000 и больше подпсоты
+            }
+            else if(count in 500..999) {
+                bindingClass.textWhoAreYou.text = "ммм у тебя $count подписчиков, но ты всё равно лох" //если 500 до 1000
+            }
+            else if(count in 100..499) {
+                bindingClass.textWhoAreYou.text = "у тебя больше 100 подписчиков, целых $count но ты лох" //от 100 и до 500
+            }
+            else if (count in 0..100) {
+                bindingClass.textWhoAreYou.text = "у тебя до 100 подписчиков, вообще иди отсюда лох" //до 100 подписоты
+            } else {
+                bindingClass.textWhoAreYou.text = "ВВЕДЕНЫ НЕВЕРНЫЕ ДАННЫЕ, ВВЕДИ ЧИСЛО" //до 100 подписоты
+            }
+        }
+
+        bindingClass.buttonPreviousActivity.setOnClickListener {
             val intent = Intent(this@SecondEmptyActivity, MainActivity::class.java)
             startActivity(intent)
         }
+
     }
     override fun onStart() {
         super.onStart()
@@ -33,5 +57,20 @@ class SecondEmptyActivity : AppCompatActivity() {
     override fun onResume() {
         super.onResume()
         Log.d(TAG, "onResume")
+    }
+
+    override fun onPause() {
+        super.onPause()
+        Log.d(TAG, "onPause")
+    }
+
+    override fun onStop() {
+        super.onStop()
+        Log.d(TAG, "onStop")
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        Log.d(TAG, "onDestroy")
     }
 }
