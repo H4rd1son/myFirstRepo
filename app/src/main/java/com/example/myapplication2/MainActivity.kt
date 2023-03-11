384fc01db065d35eba1b5462b1eb373ed4a28bd7
Hardison <hardison@xaker.ru>
2023-03-11 18:38:08 +0300

    15.02.2023 add EditText, setText from EditText to textView, add Button for EditText 15.02.2023 rename var a:Int = 0 to var count 15.02.2023 add Log fun for button4 17.02.2023 add AlertDialog to button4 20.02.2023 add translate for @+id/editText1, add 2nd activity 21.02.2023 неосилил пока Basic Acticvity, необходимо разобраться с переходами между активностями, добавил EmptyActivity, пока потренируюсь на нем 27.02.2023 Стёр кнопки со второй активити, добавил фрагмент и логи на вторую активити 02.03.2023 Удалил все неиспользуемые и нерабочие классы 06.03.2023 воспользовался ViewBinding, создал класс для разметки, используя lateinit var bindingClass. Переписал некоторые кнопки используя созданный клас. Избавился от локальных переменных в классе MainActivity 08.03.2023 убрал фрагмент со второго активити, добавил кнопку, 2 textView, EditText. При вводе числа, в зависимости от числа отображается текст. 11.03.23 перестал работать build.gradle, поправил его работу, теперь приложение снова собирается и запускается

--- app/src/main/java/com/example/myapplication2/MainActivity.kt
+++ app/src/main/java/com/example/myapplication2/MainActivity.kt
@@ -4,9 +4,6 @@ package com.example.myapplication2
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.View
-import android.widget.EditText
-import android.widget.TextView
 import android.widget.Toast
 import androidx.appcompat.app.AlertDialog
 import com.example.myapplication2.databinding.ActivityMainBinding
