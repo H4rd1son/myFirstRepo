82287f2eea9445d46e67b89be2de759ce204e663
Hardison <hardison@xaker.ru>
2023-03-15 18:44:30 +0300

    15.02.2023 add EditText, setText from EditText to textView, add Button for EditText 15.02.2023 rename var a:Int = 0 to var count 15.02.2023 add Log fun for button4 17.02.2023 add AlertDialog to button4 20.02.2023 add translate for @+id/editText1, add 2nd activity 21.02.2023 неосилил пока Basic Acticvity, необходимо разобраться с переходами между активностями, добавил EmptyActivity, пока потренируюсь на нем 27.02.2023 Стёр кнопки со второй активити, добавил фрагмент и логи на вторую активити 02.03.2023 Удалил все неиспользуемые и нерабочие классы 06.03.2023 воспользовался ViewBinding, создал класс для разметки, используя lateinit var bindingClass. Переписал некоторые кнопки используя созданный клас. Избавился от локальных переменных в классе MainActivity 08.03.2023 убрал фрагмент со второго активити, добавил кнопку, 2 textView, EditText. При вводе числа, в зависимости от числа отображается текст. 11.03.23 перестал работать build.gradle, поправил его работу, теперь приложение снова собирается и запускается. Дал более понятное название переменным. 14.03.23 добавил 3й экран активити, добавляю как бы поля логина и пароля. 15.03.23 Добавил константы на логин и пароль в 3м активити, связал работу двух полей для проверки логина и пароля

---
+++ app/src/main/java/com/example/myapplication2/constance/Constance.kt
@@ -0,0 +1,15 @@
+package com.example.myapplication2.constance
+
+object Constance {
+    const val ADMIN_LOGIN = "Admin"
+    const val ROOT_LOGIN = "root"
+    const val USER_LOGIN = "User"
+
+    const val ADMIN_PASS = 4321
+    const val ROOT_PASS = 4859
+    const val USER_PASS = 1234
+
+    const val ADMIN_HELLO = "Успешный логин. Приветсвую Admin"
+    const val ROOT_HELLO = "Успешный логин. Приветсвую ROOT"
+    const val USER_HELLO = "Успешный логин. Приветсвую User"
+}
