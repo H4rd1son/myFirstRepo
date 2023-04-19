47407f9886f59d81bc14b8831cdbc604b6267916
Hardison <hardison@xaker.ru>
2023-04-19 13:57:55 +0300

    15.02.2023 add EditText, setText from EditText to textView, add Button for EditText 15.02.2023 rename var a:Int = 0 to var count 15.02.2023 add Log fun for button4 17.02.2023 add AlertDialog to button4 20.02.2023 add translate for @+id/editText1, add 2nd activity 21.02.2023 неосилил пока Basic Acticvity, необходимо разобраться с переходами между активностями, добавил EmptyActivity, пока потренируюсь на нем 27.02.2023 Стёр кнопки со второй активити, добавил фрагмент и логи на вторую активити 02.03.2023 Удалил все неиспользуемые и нерабочие классы 06.03.2023 воспользовался ViewBinding, создал класс для разметки, используя lateinit var bindingClass. Переписал некоторые кнопки используя созданный клас. Избавился от локальных переменных в классе MainActivity 08.03.2023 убрал фрагмент со второго активити, добавил кнопку, 2 textView, EditText. При вводе числа, в зависимости от числа отображается текст. 11.03.23 перестал работать build.gradle, поправил его работу, теперь приложение снова собирается и запускается. Дал более понятное название переменным. 14.03.23 добавил 3й экран активити, добавляю как бы поля логина и пароля. 15.03.23 Добавил константы на логин и пароль в 3м активити, связал работу двух полей для проверки логина и пароля 16.03.23 добавил отображение картинки при удачном вводе пары логин и пароль, начал выносить when из SalaryActivity в отдельную функцию 18.03.23 настроил корректное отображение картинок в зависимости от введенных данных логина и пароля 19.03.23 Вынес код сверки логина и пароля в отдельную функцию на SalaryActivity 21.03.23 Добавил landscape разметку для SalaryActivity 25.03.23 Добавил кнопку регистрации и ввел регистрацию новой пары логин и пароль в переменные, но после onDestroy данные нового логина и пароля не сохраняются. Данные inputNumberFiled передаются в textViewYouText с SecondEmptyActivity передаются на MainActivity при нажатии buttonPreviousActivity 27.03.2023 Перенес функцию accessCheckIf в класс ButtonsFunc, присутствует успешный логин при пустых полях логина и пароля из-за значений по умолчанию в переменных для нового пользователя. 13.04.2023 Создавал бесполезные активити, щупал массивы, заменил старый метод передачи данных между активити на актуальный callback 19.04.2023 Создал разметку для menu, main_menu, наполнил второе активити. Активировал home button в supportActionBar. Добавил Toast на кнопки в menu.

--- app/src/main/java/com/example/myapplication2/SecondEmptyActivity.kt
+++ app/src/main/java/com/example/myapplication2/SecondEmptyActivity.kt
@@ -4,6 +4,9 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.view.Menu
+import android.view.MenuItem
+import android.widget.Toast
 import com.example.myapplication2.databinding.SecondEmptyActivityBinding
 
 class SecondEmptyActivity : AppCompatActivity() {
@@ -17,7 +20,8 @@ class SecondEmptyActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         bindingClass = SecondEmptyActivityBinding.inflate(layoutInflater)
         setContentView(bindingClass.root)
-
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+        supportActionBar?.title = "Second Activity"
         Log.d(TAG, "onCreate")
 //        val count = bindingClass.inputNumberField.getText().toString()
 
@@ -30,22 +34,27 @@ class SecondEmptyActivity : AppCompatActivity() {
             else if(count.toInt() >= 1000) {
                 val tempText = "красава у тебя $count подписчиков, но ты всё равно лох"
                 bindingClass.textWhoAreYou.text = tempText //если 1000 и больше подпсоты
+                supportActionBar?.title = "Подписоты: $count"
             }
             else if(count.toInt() in 500..999) {
                 val tempText = "ммм у тебя $count подписчиков, но ты всё равно лох"
                 bindingClass.textWhoAreYou.text =  tempText//если 500 до 1000
+                supportActionBar?.title = "Подписоты: $count"
             }
             else if(count.toInt() in 100..499) {
                 val tempText = "у тебя больше 100 подписчиков, целых $count но ты лох"
                 bindingClass.textWhoAreYou.text =  tempText//от 100 и до 500
+                supportActionBar?.title = "Подписоты: $count"
             }
             else if (count.toInt() in 0..100) {
                 val tempText = "у тебя до 100 подписчиков, вообще иди отсюда лох"
                 bindingClass.textWhoAreYou.text =  tempText//до 100 подписоты
+                supportActionBar?.title = "Подписоты: $count"
             }
             else {
                 val tempText = "ВВЕДЕНЫ НЕВЕРНЫЕ ДАННЫЕ, ВВЕДИ ЧИСЛО"
                 bindingClass.textWhoAreYou.text =  tempText//ошибка не отрабатывает
+                supportActionBar?.title = "ОШИБКА"
             }
         }
 
@@ -62,6 +71,29 @@ class SecondEmptyActivity : AppCompatActivity() {
             startActivity(intent)
         }
     }
+
+    //    функция которая загружает меню из разметки main_menu, она есть у активити
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.main_menu, menu) //наполняем разметку меню с помощью menuInflater, указываем куда мы хотим поместить
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when(item.itemId){
+            android.R.id.home -> finish()
+            R.id.sync -> {
+                Toast.makeText(this, "Sync", Toast.LENGTH_SHORT).show()
+            }
+            R.id.search -> {
+                Toast.makeText(this, "Search", Toast.LENGTH_SHORT).show()
+            }
+            R.id.save -> {
+                Toast.makeText(this, "Save", Toast.LENGTH_SHORT).show()
+            }
+        }
+        return true
+    }
+
     override fun onStart() {
         super.onStart()
         Log.d(TAG, "onStart")
